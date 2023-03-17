87f319966287cd6e81c8560fa39ee328117cb7f3
Andrey Ka <tork713@gmail.com>
2023-03-17 18:44:18 +0700

    строки и значения, конкатенация

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ def main():
         ch = 'X'
     print(name)
 
+    my_string = 'Ромашки белые'
+    size = len(my_string)
+    ch = my_string[6]
+    print(ch)
+    print(my_string[2], my_string[4], my_string[6])
+    print(size)
+
+
+
 
 
 if __name__ == '__main__':
